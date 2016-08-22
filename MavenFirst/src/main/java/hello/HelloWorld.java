6bc011ab899f5c7a10df4065a4db9e89609dba37
rakeshme87 <rakesh.me87@gmail.com>
2016-08-22 18:01:31 +0530

    Add files via upload

---
+++ MavenFirst/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package main.java.hello;
+
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+
+	}
+
+}
